7254b403c83553b152e3ef644e3e4b54610c70f3
Sudipta Pandit <realsdx0@gmail.com>
2018-06-14 17:32:07 +0530

    Exception handling in views.track

--- iplogger/views.py
+++ iplogger/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
 from django.shortcuts import render
-from .forms import TrackingCodeForm
-from .models import TrackingCode, Log
+from iplogger.forms import TrackingCodeForm
+from iplogger.models import TrackingCode, Log
 from random import randint
 from django.contrib.auth import get_user_model
-from .forms import UserCreationForm
+from iplogger.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 #Django sucks here, Need to import the custom User model explicitly, even though u updated th settings
@@ -33,9 +33,14 @@ def track(req, tracking_code):
     info = {}
     required_headers=['REMOTE_ADDR','HTTP_HOST','HTTP_USER_AGENT','HTTP_HOST','HTTP_REFERER']
     for keys in required_headers:
-        info[keys]=req.META[keys]
+        try:
+            info[keys]=req.META[keys]
+        except KeyError:
+            info[keys]='N/A'
+    #Save logs in any case (it may be same) and count hits
     
-    return render(req,'iplogger/track.html',{'ipinfo':info})
+    
+    return render(req,'iplogger/track.html',{})
 
 
 def results(req, tracking_code):
