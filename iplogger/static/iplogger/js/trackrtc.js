0ec33a73561291c85247b5d3d0e0297169d5fe51
SDX <realsdx@protonmail.com>
2018-12-08 20:04:01 +0530

    [UPDATE] trackjs

---
+++ iplogger/static/iplogger/js/trackrtc.js
@@ -0,0 +1,13 @@
+var sd;
+window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;   //compatibility for firefox and chrome
+    var pc = new RTCPeerConnection({iceServers:[]}), noop = function(){};      
+    pc.createDataChannel("");    //create a bogus data channel
+    pc.createOffer(pc.setLocalDescription.bind(pc), noop);    // create offer and set local description
+    pc.onicecandidate = function(ice){  //listen for candidate events
+        if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
+        var myIP = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+        sd = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate);
+        console.log('my IP: ', myIP);
+        console.log('data==', sd);
+        pc.onicecandidate = noop;
+    };
